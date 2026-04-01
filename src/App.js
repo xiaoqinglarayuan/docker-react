01ccfacb7119360951ebbb796908d319f9801361
Lara <xyuan93@gatech.edu>
2026-04-01 08:44:57 -0700

    revert app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the Feature branch
+          Learn React
         </a>
       </header>
     </div>
